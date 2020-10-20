4ddd8d822fc946f02a39878f6baa2c5a2396e1f8
liudonghua123 <liudonghua123@gmail.com>
2020-10-20 13:15:54 +0800

    config window size only for desktop platform

--- lib/main.dart
+++ lib/main.dart
@@ -11,21 +11,27 @@ import 'package:window_size/window_size.dart' as window_size;
 FlutterConfiguration config;
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  // await DesktopWindow.setWindowSize(Size(500, 300));
-  var window = await window_size.getWindowInfo();
-  if (window.screen != null) {
-    final screenFrame = window.screen.visibleFrame;
-    final width = 600.0;
-    final height = 500.0;
-    final left = ((screenFrame.width - width) / 2).roundToDouble();
-    final top = ((screenFrame.height - height) / 3).roundToDouble();
-    final frame = Rect.fromLTWH(left, top, width, height);
-    window_size.setWindowFrame(frame);
-    window_size.setWindowMinSize(Size(1.0 * width, 1.0 * height));
-    window_size.setWindowMaxSize(Size(2.0 * width, 2.0 * height));
-    window_size.setWindowTitle('System Network Proxy (${Platform.operatingSystem})');
-  }
+  await configWindowSizeIfNecessary();
   config = await FlutterConfiguration.fromAsset('assets/config.yaml');
   await Service().init();
   runApp(App());
+}
+
+configWindowSizeIfNecessary() async {
+  if (Platform.isMacOS || Platform.isLinux || Platform.isWindows) {
+    // await DesktopWindow.setWindowSize(Size(500, 300));
+    var window = await window_size.getWindowInfo();
+    if (window.screen != null) {
+      final screenFrame = window.screen.visibleFrame;
+      final width = 600.0;
+      final height = 500.0;
+      final left = ((screenFrame.width - width) / 2).roundToDouble();
+      final top = ((screenFrame.height - height) / 3).roundToDouble();
+      final frame = Rect.fromLTWH(left, top, width, height);
+      window_size.setWindowFrame(frame);
+      window_size.setWindowMinSize(Size(1.0 * width, 1.0 * height));
+      window_size.setWindowMaxSize(Size(2.0 * width, 2.0 * height));
+      window_size.setWindowTitle('System Network Proxy (${Platform.operatingSystem})');
+    }
+  }
 }
